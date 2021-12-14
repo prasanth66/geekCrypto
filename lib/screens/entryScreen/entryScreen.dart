1f7e2acfe9abd266363febf413972aa3cb8cd730
kundella prasanth yadav <prasanthkdp6@gmail.com>
2021-12-15 01:10:22 +0530

    work on entry screen

---
+++ lib/screens/entryScreen/entryScreen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
+
+class EntryScreen extends StatelessWidget {
+   EntryScreen({Key? key}) : super(key: key);
+   static const imagesList = [
+     "assets/images/bitcoin1.png",
+     "assets/images/bitcoin2.png",
+     "assets/images/bitcoin3.png",
+   ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 100.0,),
+          Text(
+            "WELECOME TO ""CRYPTO GEEK",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25
+            ),
+          ),
+          SizedBox(height: 20.0,),
+          Expanded(
+            child: Swiper(
+              itemBuilder: (BuildContext context,int index){
+                return Image.asset(imagesList[index],fit: BoxFit.fill,);
+              },
+              itemCount: imagesList.length,
+              pagination: SwiperPagination(),
+              // control: SwiperControl(),
+            ),
+          ),
+          SizedBox(height: 20.0,),
+          ElevatedButton(
+              onPressed: (){},
+              child: Text("GET STARTED")
+          ),
+          SizedBox(height: 100.0,),
+        ],
+      )
+
+    );
+  }
+}
